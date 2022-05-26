d2f4caff44dc90e8bc55f29ec2eee2288931b250
Hassan Shan <55858014+HassanShanJava@users.noreply.github.com>
2022-05-27 02:10:08 +0500

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -49,7 +49,7 @@ function TodoList() {
   
     return (
     <div>
-        <h1>React-Project1-TodoList</h1>
+        <h1>Hassan's List</h1>
         <TodoForm onSubmit={addTodo}/>
         <Todo todos={todos}
         completeTodo={completeTodo}
